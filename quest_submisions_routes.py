9a0ec364ca061fc1dbafa2361c01380f64d41e96
karastoyanov on macbookPRO <karastoqnov.alexandar@gmail.com>
2025-07-05 12:51:19 +0300

    <ench>(Added JS quest submission):
    - updated JS code execution

--- quest_submisions_routes.py
+++ quest_submisions_routes.py
@@ -1,11 +1,11 @@
 import logging
-import uuid, os, requests, json
+import uuid, os, requests, json, re
 from dotenv import load_dotenv
 from flask import Blueprint, request, jsonify
 from extensions import db
 from services import token_required
 from sqlalchemy import text
-from models import QuestSolution
+from models import Quest, QuestSolution
 
 from user_progress_func import update_xp
 
@@ -33,89 +33,96 @@ def quest_solution(quest_id):
         
     """
     
-    # Get the code, language, and user_id from the request
+    # Get the quest, code, language, and user_id from the request
     code = request.json.get('code')
     language = request.json.get('language')
-    quest_xp = db.session.execute(text("SELECT xp FROM coding_quests WHERE id = :quest_id"), {'quest_id': quest_id}).scalar()
-    
-    # Get the test cases and expected results from the database(in raw format)
-    test_cases_raw = db.session.execute(text("""SELECT test_inputs FROM coding_quests WHERE id = :quest_id"""), {"quest_id": quest_id}).fetchone()[0]
-    expected_results_raw = db.session.execute(text("""SELECT test_outputs FROM coding_quests WHERE id = :quest_id"""), {"quest_id": quest_id}).fetchone()[0]
+    quest_xp = db.session.query(Quest).filter_by(id=quest_id).first().xp
     user_id = request.json.get('user_id')
+    
+    quest = db.session.query(Quest).filter_by(id=quest_id).first()
+    if not quest:
+        return jsonify({"error": "Quest not found"}), 404
+    
+    # Max number of tests to run
+    MAX_TESTS = 10
 
+    # Hold all the results of the tests and generate UUID for execution
+    all_results = {}
     successful_tests = 0
     unsuccessful_tests = 0
     zero_tests = [] # Hold the first example test input and putput
     zero_tests_outputs = [] # Hold the first example after executing the user code (stdout & stderr)
     execution_id = str(uuid.uuid4())
     
-    # Rework the test cases
-    test_cases = []
-    for line in test_cases_raw.strip().splitlines():
-        try:
-            parsed = json.loads(line)
-            args = " ".join(item[0] for item in parsed if isinstance(item, list) and item)
-            test_cases.append(args)
-        except (json.JSONDecodeError, IndexError, TypeError) as e:
-            raise ValueError(f"Invalid test case line: {line} → {e}")
-
-    # Rework the expected results
-    expected_results = []
-    for line in expected_results_raw.strip().splitlines():
-        try:
-            parsed = json.loads(line)
-            expected_results.append(parsed[0])  # ["text"] → "text"
-        except (json.JSONDecodeError, IndexError, TypeError) as e:
-            raise ValueError(f"Invalid expected result line: {line} → {e}")
-    
-    # Hold all the results of the tests
-    all_results = {}
-    
-    # Send the code to the Piston API for execution(iterate through the test cases)
-    for test in range(len(test_cases)):
-        current_input = test_cases[test]
-        current_expected_result = expected_results[test]
-
+    for i in range(MAX_TESTS):
+        input_attr = getattr(quest, f'input_{i}', None)
+        output_attr = getattr(quest, f"output_{i}", None)
+        print(f"Running test {i+1} with input: {input_attr} and expected output: {output_attr}")
         
-        data = {
-            "language": language,
-            "version": "*",
-            "files": [
-                {
-                    "name": f"{user_id}_{quest_id}.{language}",
-                    "content": code
-                }
-            ],
-            "stdin": current_input,
-            "args": [],
-            "compile_timeout": 5000,
-            "run_timeout": 2000,
-            "compile_memory_limit": -1,
-            "run_memory_limit": -1
-        }
+        if language != 'javascript':
+            data = {
+                "language": language,
+                "version": "*",
+                "files": [
+                    {
+                        "name": f"{user_id}_{quest_id}.{language}",
+                        "content": code
+                    }
+                ],
+                "stdin": input_attr,
+                "args": [],
+                "compile_timeout": 5000,
+                "run_timeout": 2000,
+                "compile_memory_limit": -1,
+                "run_memory_limit": -1
+            }
+        else:
+            function_name = re.search(r'function\s+(\w+)\s*\(', code)
+            print(f"Function name found: {function_name.group(1) if function_name else 'None'}")
+            data = {
+                "language": language,
+                "version": "*",
+                "files": [
+                    {
+                        "name": f"{user_id}_{quest_id}.{language}",
+                        "content": code + '\n' + f"console.log({function_name.group(1)}(process.argv[2]));"
+                    }
+                ],
+                "stdin": "",
+                "args": [input_attr],
+                "compile_timeout": 5000,
+                "run_timeout": 2000,
+                "compile_memory_limit": -1,
+                "run_memory_limit": -1,
+                "execution_id": execution_id
+            }
+            print(code + '\n' + f"{function_name.group(1)}(process.argv[2]);")
         
+        # Send the code to the Piston API for execution
         exec_url = os.getenv("PISTON_API_URL") + '/api/v2/execute'
         response = requests.post(exec_url, json=data)
-
+        
+        print("Data sent to Piston API:", json.dumps(data, indent=2))
+        
         # Check if the response is successful and process the results
         if response.status_code == 200:
             current_output = response.json()['run']['stdout'].strip()
             current_error = response.json()['run']['stderr'].strip()
-            
-            if str(current_output) == str(current_expected_result):
+        
+            if str(current_output) == str(output_attr):
                 successful_tests += 1
             else:
                 unsuccessful_tests += 1
             
-            if test == 0:
-                zero_tests.append(current_input)
-                zero_tests.append(current_expected_result)
+            if i == 0:
+                zero_tests.append(input_attr)
+                zero_tests.append(output_attr)
                 zero_tests_outputs.append(current_output)
                 zero_tests_outputs.append(current_error)
             
-            all_results.update({f"Test {test+1}": {"input": current_input, 
+            all_results.update({f"Test {i+1}": {"input": input_attr, 
                                                 "output": current_output, 
-                                                "expected_output": current_expected_result, 
+                                                "expected_output": output_attr, 
                                                 "error": current_error}})
 
         # If the response is not successful, handle the error    
@@ -123,7 +130,7 @@ def quest_solution(quest_id):
             message = response.json().get('message', 'Unknown error')
             logs_message = response.json()
             successful_tests = 0
-            unsuccessful_tests = len(test_cases)
+            unsuccessful_tests = i
             zero_tests.append("")
             zero_tests.append("")
             zero_tests_outputs.append("")
@@ -133,26 +140,24 @@ def quest_solution(quest_id):
                 "logs": logs_message
             }), 500
 
+
     # Check if there are any successful or unsuccessful tests
     if not unsuccessful_tests:
         message = 'Congratulations! Your solution is correct!'
         
         # Update the quest solved times
         try:
-            db.session.execute(
-                text("UPDATE coding_quests SET solved_times = solved_times + 1 WHERE id = :quest_id"),
-                {'quest_id': quest_id}
-            )
+            quest.solved_times = (quest.solved_times or 0) + 1
             db.session.commit()
         except Exception as e:
             db.session.rollback()
         
         # Update user XP if not already solved
         # Check if the user has already solved this quest
-        existing_solution = db.session.execute(
-            text("SELECT * FROM quest_solutions WHERE quest_id = :quest_id AND user_id = :user_id"),
-            {'quest_id': quest_id, 'user_id': user_id}
-        ).fetchone()
+        existing_solution = db.session.query(QuestSolution).filter_by(
+            quest_id=quest_id,
+            user_id=user_id
+        ).first()
         if existing_solution is None or not existing_solution.is_solved:
             # Only update XP if the quest was not solved before
             update_xp(user_id, quest_xp) 
@@ -170,7 +175,7 @@ def quest_solution(quest_id):
             language=language,
             tests_passed=successful_tests,
             tests_failed=unsuccessful_tests,
-            is_solved=(successful_tests == len(test_cases))
+            is_solved=(successful_tests == i)
         )
         db.session.add(new_solution)
         db.session.commit()
